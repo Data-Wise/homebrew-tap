2d7dae28da65da2fadb8693580e0b49cd0b16d2c
Davood Tofighi <dtofighi@gmail.com>
2026-01-29 10:49:34 -0700

    flow-cli: update to v5.22.0 (#66)
    Co-authored-by: Data-Wise <1749560+Data-Wise@users.noreply.github.com>

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -4,8 +4,8 @@
 class FlowCli < Formula
   desc "ZSH workflow tools designed for ADHD brains"
   homepage "https://data-wise.github.io/flow-cli/"
-  url "https://github.com/Data-Wise/flow-cli/archive/refs/tags/v5.21.0.tar.gz"
-  sha256 "b99c04fe333fe19f8ca173469fccbe876e9738a3a12160d8cef6f91aaea80394"
+  url "https://github.com/Data-Wise/flow-cli/archive/refs/tags/v5.22.0.tar.gz"
+  sha256 "bbe8e7bbb5b6a47dcab16d453512a4b9c4eb09f9ac57a526277ddf9303575ee6"
   license "MIT"
   head "https://github.com/Data-Wise/flow-cli.git", branch: "main"
 
