268e36782048ef2a5d216c815a40fe7860bcbfd8
Data-Wise <1749560+Data-Wise@users.noreply.github.com>
2026-02-04 05:43:05 +0000

    flow-cli: update to v6.4.0

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -4,8 +4,8 @@
 class FlowCli < Formula
   desc "ZSH workflow tools designed for ADHD brains"
   homepage "https://data-wise.github.io/flow-cli/"
-  url "https://github.com/Data-Wise/flow-cli/archive/refs/tags/v6.3.0.tar.gz"
-  sha256 "0d9750adb3924561182e112c55bc66abc6e83951bc6284b9ede53506e97f6c00"
+  url "https://github.com/Data-Wise/flow-cli/archive/refs/tags/v6.4.0.tar.gz"
+  sha256 "37f8eb5720bd18ddae93e70e8f0e3624f6647acc793bada39156e7ea6104033d"
   license "MIT"
   head "https://github.com/Data-Wise/flow-cli.git", branch: "main"
 
